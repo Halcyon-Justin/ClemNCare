8be99a7a513c2260953b35991813b47911cd6571
Justin <halcyon.justin@gmail.com>
2023-12-10 00:02:18 -0600

    CORS for testing

--- app/src/main/java/halcyon/clemncare/app/controller/FamilyController.java
+++ app/src/main/java/halcyon/clemncare/app/controller/FamilyController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ public class FamilyController {
     @Autowired
     private FamilyService familyService;
 
+    @CrossOrigin
     @GetMapping("/")
     public ResponseEntity<Object> getFamilies() {
         return ResponseHandler.responseBuilder("Requested All Family Data", HttpStatus.OK,
